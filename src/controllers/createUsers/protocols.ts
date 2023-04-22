8f4dbc61742935be0a0e862519ee6ca59aadec5e
Junior580 <junior_silvapereira@hotmail.com>
2023-04-22 14:55:04 -0300

    feat: add mongoCreate user repository

---
+++ src/controllers/createUsers/protocols.ts
@@ -0,0 +1,13 @@
+import { IUser } from '../../models/Users'
+
+export interface ICreateUserParams{
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
+}
+
+export interface ICreateUserRepository {
+  createUser(params:ICreateUserParams): Promise<IUser>
+}
---
+++ src/repositories/createUsers/MongoCreateUsers.ts
@@ -0,0 +1,24 @@
+import { mongoClient } from '../../database/mongo'
+import {
+  ICreateUserParams,
+  ICreateUserRepository,
+} from '../../controllers/createUsers/protocols'
+import { IUser } from '../../models/Users'
+
+export class MongoCreateUser implements ICreateUserRepository {
+  async createUser(params: ICreateUserParams): Promise<IUser> {
+    const { insertedId } = await mongoClient.db
+      .collection('users')
+      .insertOne(params)
+
+    const user = await mongoClient.db
+      .collection<Omit<IUser, 'id'>>('users')
+      .findOne({ _id: insertedId })
+
+    if (!user) throw new Error('User not created')
+
+    const { _id, ...rest } = user
+
+    return { id: _id.toHexString(), ...rest }
+  }
+}
