1fbff1014011338261b14b655f88fb45b248bc51
Junior580 <junior_silvapereira@hotmail.com>
2023-05-01 16:30:36 -0300

    feat: add routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import express from 'express'
 import { config } from 'dotenv'
-import { GetUsersController } from './controllers/getUsers/GetUsers'
-import { MongoGetUsersRepository } from './repositories/getUsers/MongoGetUsers'
 import { mongoClient } from './database/mongo'
-import { MongoCreateUserRepository } from './repositories/createUsers/MongoCreateUsers'
-import { CreateUserController } from './controllers/createUsers/createUsers'
-import { MongoUpdateUserRepository } from './repositories/updateUser/MongoUpdateUser'
-import { UpdateUserController } from './controllers/updateUser/UpdateUser'
-import { MongoDeleteUserRepository } from './repositories/deteleUser/MongoDeleteUser'
-import { DeleteUserController } from './controllers/deleteUser/DeleteUserController'
 import { routes } from './routes/routes'
 
 const main = async () => {
